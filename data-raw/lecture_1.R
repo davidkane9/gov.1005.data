5bda5094827f960d6899d2f3fa6d190a8bdb5c7b
David Kane <dave.kane@gmail.com>
2018-08-29 08:35:57 -0400

    elections data

--- data-raw/lecture_1.R
+++ data-raw/lecture_1.R
@@ -1,1 +1,12 @@
 # Script for creating data for Lecture 1.
+
+# I am using data from
+
+# See http://qss.princeton.press/ for details.
+
+library(tidyverse)
+x <- read_csv("data-raw/elections.csv")
+x$rep_per <- x$rep / (x$rep + x$dem + x$other)
+
+elections <- x
+usethis::use_data(elections)
