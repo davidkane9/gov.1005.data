408a89e76d42e87fa74b2ffde49d64610f0c77b2
David Kane <dave.kane@gmail.com>
2018-09-13 07:17:24 -0400

    adding vote data from 2016

---
+++ R/votes_2016.R
@@ -0,0 +1,7 @@
+#' County Presidential Election Results for 2016
+#'
+#' A dataset presidential election votes for each US county.
+#'
+#' @format A data frame with 3,112 rows and 39 variables.
+#' @source Courtesy of Xiang Zhou and Gov 2000.
+"votes_2016"
---
+++ data-raw/pelection2016_prep.R
@@ -0,0 +1,7 @@
+# Data from Gov 2000. Thanks Xiang! Leaving it messy on purpose.
+
+load("data-raw/pelection2016.RData")
+
+votes_2016 <- pelection2016
+
+# usethis::use_data(votes_2016)
