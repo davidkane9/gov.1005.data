1def5a0d93c248df5d863f2400a3120734a2dc38
David Kane <dave.kane@gmail.com>
2018-09-17 09:16:40 -0400

    set the data as a tibble

--- data-raw/pelection2016_prep.R
+++ data-raw/pelection2016_prep.R
@@ -1,7 +1,9 @@
 # Data from Gov 2000. Thanks Xiang! Leaving it messy on purpose.
 
+library(tidyverse)
+
 load("data-raw/pelection2016.RData")
 
-votes_2016 <- pelection2016
+votes_2016 <- as_tibble(pelection2016)
 
-# usethis::use_data(votes_2016)
+# usethis::use_data(votes_2016, overwrite = TRUE)
