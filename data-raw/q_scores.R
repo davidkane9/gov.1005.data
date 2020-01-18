dd1236858c4729ea532a44afe236024c68c7e2bf
David Kane <dave.kane@gmail.com>
2020-01-18 16:49:58 -0500

    new version with a much cleaner subset of the class data.

--- data-raw/q_scores.R
+++ data-raw/q_scores.R
@@ -15,10 +15,22 @@ x <- read_json("q_scores.json", simplifyDataFrame = TRUE, flatten = TRUE) %>%
          number = evaluations_amount) %>%
   mutate(overall = parse_number(overall, na = "None"),
          workload = parse_number(workload, na = "None"),
-         number = parse_number(number, na = "None"))
+         number = parse_number(number, na = "None")) %>%
+
+  # Adding more clean up in Jan 2020. Want a much cleaner version of the data so
+  # that it is easier for students to work with in the first few weeks.
+
+  filter(course_component == "Lecture") %>%
+  filter(! course_level %in% c("Graduate Course", "N/A")) %>%
+  select(-divisional_distribution, -course_level, -course_component, -exam) %>%
+  drop_na() %>%
+  distinct() %>%
+  distinct(name, .keep_all= TRUE) %>%
+  filter(number > 25)
+
 
 # Code for saving object
 
 qscores <- x
-usethis::use_data(qscores)
+usethis::use_data(qscores, overwrite = TRUE)
 
