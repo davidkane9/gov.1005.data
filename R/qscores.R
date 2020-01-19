634c1c08fd0fe74f52b354aee8a1d28b81f115e1
David Kane <dave.kane@gmail.com>
2020-01-18 20:41:09 -0500

    cleaned up and documented data

--- R/qscores.R
+++ R/qscores.R
@@ -1,5 +1,16 @@
-#' Scores from the Harvard Q-Guide
+#' Scores from the Harvard Q Guide
 #'
-#' Thanks to Benny Chang '22 for this data
+#' Thanks to Benny Chang '22 for this data.
+#'
+#' @format A tibble with 141 observations and 6 variables:
+#' \describe{
+#'   \item{names}{Course full name}
+#'   \item{course}{Course number}
+#'   \item{department}{Course department}
+#'   \item{overall}{average overall student evaluation on a scale of 1 to 5}
+#'   \item{workload}{average weekly workload in hours}
+#'   \item{number}{total number of students filling out evaluation forms}
+#' }
+#' @source \url{http://q-guide-plus.appspot.com/}
 #'
 "qscores"
