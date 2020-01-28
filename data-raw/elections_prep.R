09af8e86b422b425b27a5f96615ec18bf05c5908
David Kane <dave.kane@gmail.com>
2020-01-27 20:43:11 -0500

    Adding some data stuff which I have failed to finish. Annoying. Have a CA fix this properly!

--- data-raw/elections_prep.R
+++ data-raw/elections_prep.R
@@ -1,11 +1,22 @@
 # Script for creating elections data
 
-# I am using data from QSS by Imai.
+# I am using data from MIT Election Data project. Why is so hard to find a
+# simple, free dataset of this? Five elections is OK, but not that good.
 
-# See http://qss.princeton.press/ for details.
 
 library(tidyverse)
-x <- read_csv("data-raw/elections.csv")
+load("countypres_2000-2016.RData")
+
+raw <- x
+
+x <- raw %>%
+  as_tibble() %>%
+  select(-state_po, -FIPS, -office, -version, -candidate, -totalvotes) %>%
+  filter(party %in% c("democrat", "republican")) %>%
+  pivot_wider(names_from = party,
+              values_from = candidatevotes) %>%
+  mutate(votes = republican + democrat)
+
 x$rep_per <- x$rep / (x$rep + x$dem + x$other)
 
 elections <- x
